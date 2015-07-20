15258a6774c59ec54fcc6eca84861afc3d54932e
dingjunnan <1415609649@qq.com>
2015-07-20 12:50:53 +0800

    modify generateQrcode.php

--- generateQrcode.php
+++ generateQrcode.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = "JXamYh-5F9gmMmnciDAY22cDJgpavqEtuiTZK0j_ZfzEVWpibmeY_huTIAOXfweZuZe4EfAbF2Doa2kPWHiLxXkTY7DWDWrXfkjydQBPbko";
+/*$access_token = "JXamYh-5F9gmMmnciDAY22cDJgpavqEtuiTZK0j_ZfzEVWpibmeY_huTIAOXfweZuZe4EfAbF2Doa2kPWHiLxXkTY7DWDWrXfkjydQBPbko";
 $qrcode = '{"action_name": "QR_LIMIT_SCENE", "action_info": {"scene": {"scene_id": 1 }}}';
 $url = "https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token=".$access_token;
 $result = https_post($url,$qrcode);
@@ -58,5 +58,32 @@ function https_post($url, $data = null){
 	$output = curl_exec($curl);
 	curl_close($curl);
 	return $output;
+}*/
+require_once("config.php");
+$conn = new mysqli($host, $user, $password, $database);
+$query = "select * from T_teacher where imageUrl='0'";
+$result = $conn->query($query);
+while($row = $result->fetch_assoc()){
+	echo $row["openId"];
 }
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
