3d705bbdee40e48a83eb4c34956d1365cf445660
dingjunnan <1415609649@qq.com>
2015-07-25 17:32:53 +0800

    showTeacherInformation.php
    delete function

--- showTeacherInformation.php
+++ showTeacherInformation.php
@@ -341,8 +341,13 @@ $certificate_token = $auth2->uploadToken($bucket2,null,3600,null,true);
 	</div>
 	<div class="row">
 		<div class="col-md-4 col-md-offset-2" id="certificate_delete">
-			<label>删除/修改证书:</label>
-			<span class="fui-new"></span><span class="fui-trash"></span>
+			<label>删除/修改证书:</label><br>
+			<button type="button" class="btn btn-default btn-lg">
+  				<span class="fui-new">
+			</button>
+			<button type="button" class="btn btn-default btn-lg" style="margin-left:1px">
+				<span class="fui-trash"></span>
+			</button>
 		</div>
 	</div>
 	<div class="row">
