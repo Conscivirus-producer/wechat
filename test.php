9eb58951d29255e3f993fa5e10700e7002492c78
dingjunnan <1415609649@qq.com>
2015-07-26 11:52:52 +0800

    yunpian.php
    message

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
 	require_once("yunpian.php");
 	$yunpian = new Yunpian();
-	$yunpian->sendMessage("13048985068","【云片网】您的验证码是1234");
+	$yunpian->sendMessage("13480920685","【云片网】您的验证码是1234");
 ?>
