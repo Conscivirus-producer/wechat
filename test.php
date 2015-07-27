e99ec57e1d794878ad4ea89a757eda2a63ab2f6e
dingjunnan <1415609649@qq.com>
2015-07-27 23:44:07 +0800

    test.php
    message func

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
 	require_once("yunpian.php");
 	$yunpian = new Yunpian("9b7f2d98e5eff742fa1e8f4d921cd2f3");
-	$yunpian->sendMessage("13048985068","【云片网】您的验证码是1234");
+	$yunpian->sendMessage("13048985068","【我教你学】您好，您的免费试教已预约成功！如果您有任何疑问，请致电400-686-4616或直接微信回复。关注公众号“我教你学”，服务进度实时掌控！");
 ?>
