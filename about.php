c15cf5797eedcefd518a600c64db0c6a25e92e83
AnderLX <andrew1230@gmail.com>
2015-08-04 22:42:38 +0800

    about page ui

--- about.php
+++ about.php
@@ -21,63 +21,66 @@
 			<!--<p> 
 <img src="image/logo.jpg" width="165" height="165" /> 
 </p>-->
-			<h6 class="text-left">
-				<font color="#48C9B0">
-				“我教你学”是什么？</font>  
+			<h6 class="text-center">
+				<div style="background-color:#48C9B0; font-size: 16px; color: white;padding: 3px; border-radius: 2px; width: 280px">
+				“我教你学”是什么？</div>  
 
 			</h6>
-			<p class="text-left">
+			<p class="text-left"  style="font-size: 12px">
 				我教你学是一个由深港高校联合创办，专注于教学服务的科技创新企业，旨在打造一个人人乐用的学习服务平台，做到更精准，更智能，更高效地择优匹配师生资源。我们的师资均来自于有丰富教学经验以及专业级别认证的深圳大学学生，他们的教学质量和资质都已通过本平台进行了认证以及审核。 
 			</p>
 		</div>
+		<div class="col-xs-8 col-xs-offset-2"><img src="http://7xk9ts.com2.z0.glb.qiniucdn.com/seperator.svg" class="center-block" style="width:150px; height: 30px"/></div>
 	</div>
 	
 	<div class="row">
 		<div class="col-md-4 col-md-offset-4">
-			<h6 class="text-left">
+			<h6 class="text-center">
 
-			<div style="color:#48C9B0;">
+			<div style="background-color:#48C9B0; font-size: 16px; color: white;padding: 3px; border-radius: 2px">
 				“我教你学”的目标是什么？</div>
 			
 				
 			</h6>
-			<p class="text-left">
+			<p class="text-left"  style="font-size: 12px">
 				我们希望能通过此平台让中小学生在想要学习的时候方便快捷的找到合适的家教，同时也能为那些有一技之长的大学生提供一个展现自己能力的平台。
 			</p>
 		</div>
+		<div class="col-xs-8 col-xs-offset-2"><img src="http://7xk9ts.com2.z0.glb.qiniucdn.com/seperator.svg" class="center-block" style="width:150px; height: 30px"/></div>
 	</div>
 	
 	<div class="row">
 		<div class="col-md-4 col-md-offset-4">
-			<h6 class="text-left">
-				<font color="#48C9B0">
-				“我教你学”能为您带来什么？</font>
+			<h6 class="text-center">
+				<div style="background-color:#48C9B0; font-size: 16px; color: white;padding: 3px; border-radius: 2px">
+				“我教你学”能为您带来什么？</div>
 			</h6>
-			<p class="text-left">
+			<p class="text-left" style="font-size: 12px">
 				本平台不对教学双方收取任何形式的费用，为教学双方搭建一条简单，透明，高效的渠道。
 				</p>
-			<p class="text-left">
+			<p class="text-left"  style="font-size: 12px">
 				对中小学生来说，除了学习基本课本知识以外，还能学习自己所想要掌握的技能与兴趣，真正做到“学你所想”；
 				</p>
-			<p class="text-left">
+			<p class="text-left"  style="font-size: 12px">
 				对有一技之长的大学生来说，真正实现 “享你所学”。
 			</p>
 		</div>
+		<div class="col-xs-8 col-xs-offset-2"><img src="http://7xk9ts.com2.z0.glb.qiniucdn.com/seperator.svg" class="center-block" style="width:150px; height: 30px"/></div>
 	</div>
 	
 	<div class="row">
 		<div class="col-md-4 col-md-offset-4">
-			<h6 class="text-left">
-				<font color="#48C9B0">
-				如何使用“我教你学”？</font>
+			<h6 class="text-center">
+				<div style="background-color:#48C9B0; font-size: 16px; color: white;padding: 3px; border-radius: 2px">
+				如何使用“我教你学”？</div>
 			</h6>
-			<p class="text-left">
+			<p class="text-left"  style="font-size: 12px">
 				如果您有想学的课程，可以通过点击“免费试听”，来找到您的心仪的家教。
 			</p>
-			<p class="text-left">
+			<p class="text-left"  style="font-size: 12px">
 				在您选择心仪的老师之后，可以通过“我的记录”来查看您所选择家教的资料。
 			</p>
-			<p class="text-left">
+			<p class="text-left"  style="font-size: 12px">
 				如果您有一技之长，想要教你所学，可以回复1并填写申请表进行注册登记。
 			</p>
 			<br />
