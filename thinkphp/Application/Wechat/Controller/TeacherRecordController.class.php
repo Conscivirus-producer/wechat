3122664e5a18c06677ddbd643dd4388cb59feecd
ZhengHuahua <sven.zhenghh@gmail.com>
2015-08-22 22:38:14 +0800

    rename testing files to file with business meaning.

--- thinkphp/Application/Wechat/Controller/TeacherRecordController.class.php
+++ thinkphp/Application/Wechat/Controller/TeacherRecordController.class.php
@@ -1,18 +1,19 @@
 <?php
 namespace Wechat\Controller;
 use Think\Controller;
-class IndexController extends Controller {
+class TeacherRecordController extends Controller {
     public function index(){
     	echo "Hello, wechat!";
 		echo session('openid');
     }
 	
-	public function test(){
+	public function myClassList(){
 		$parent = D("Parent");
 		$data = $parent->select();
 		//for ($i=0;$i < count($data); $i++){
 			//echo $data[$i]["openid"];
 		//}
+		echo session('openid');
 		$this->assign("name","qop");
 		$this->display();
 	}
