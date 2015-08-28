4c767d2d377a0b407a8f913c62159193e648fbef
ZhengHuahua <sven.zhenghh@gmail.com>
2015-08-28 22:50:07 +0800

    add generate teaching record source code

--- thinkphp/Application/Common/Logic/TeachingRecordLogic.class.php
+++ thinkphp/Application/Common/Logic/TeachingRecordLogic.class.php
@@ -7,6 +7,26 @@
 			return $this->where(array("transactionId" => $transactionId))->select();
 		}
 		
+	    public function insertTeachingRecord($transactionId, $isInitial){
+	    	$transaction = D("Transaction");
+			$data = $transaction->where(array("transactionId" => $transactionId))->select();
+			$startDate = $isInitial == "Y" ? $data[0]["fixedTime"] : date('y-m-d h:i:s',time());
+	    	$weekFrequency = $data[0]["teachingFrequency"];
+			//$startdate = "2015-09-01 13:40:00";
+			$weekMap = array("1"=>"Monday", "2"=>"Tuesday", "3"=>"Wednesday", "4"=>"Thursday", "5"=>"Friday", "6"=>"Saturday", "7"=>"Sunday");
+			$weekCdeArray = explode(',', $weekFrequency);
+			for($i = 0;$i < count($weekCdeArray); $i++){
+				$teachingTime = date("Y-m-d H:i:s", strtotime("next ".$weekMap[$weekCdeArray[$i]], strtotime($startDate)));
+				$map["teachingDt"] = array('eq',$teachingTime);
+				$map["transactionId"] = array('eq',$transactionId);
+				$result = $this->where($map)->select();
+				if(count($result) == 0){
+					$data["transactionId"] = $transactionId;
+					$data["createdDt"] = date('y-m-d h:i:s',time());
+					$data["teachingDt"] = $teachingTime;
+				}
+			}
+    	}
 	}
 
 ?>
--- thinkphp/Application/InternalSupport/Controller/TeachingRecordController.class.php
+++ thinkphp/Application/InternalSupport/Controller/TeachingRecordController.class.php
@@ -2,28 +2,22 @@
 namespace InternalSupport\Controller;
 use Think\Controller;
 class TeachingRecordController extends Controller {
-    public function insertTeachingRecord(){
-    	$transactionId = "326";
-    	$str = "mon,tue,wed";
-		$startdate = "2015-09-01 13:40:00";
-		$weekMap = array("mon"=>"Monday", "tue"=>"Tuesday", "wed"=>"Wednesday", "thu"=>"Thursday", "fri"=>"Friday", "sat"=>"Saturday", "sun"=>"Sunday");
-		$weekCdeArray = explode(',', $str);
-		$teachingRecord = D("TeachingRecord");
-		for($i = 0;$i < count($weekCdeArray); $i++){
-			$teachingTime = date("Y-m-d H:i:s", strtotime("next ".$weekMap[$weekCdeArray[$i]], strtotime($startdate)));
-			$map["teachingDt"] = array('eq',$teachingTime);
-			$result = $teachingRecord->where($map)->select();
-			if(count($result) == 0){
-				$data["transactionId"] = $transactionId;
-				$data["createdDt"] = date('y-m-d h:i:s',time());
-				$data["teachingDt"] = $teachingTime;
-				echo $teachingRecord->add($data);
-			}
-		}
-    }
-	
 	public function test(){
 		$teachingRecord = D("TeachingRecord", "Logic")->test();
 	}
 	
+	public function insertTeachingRecord(){
+		$insertTeachingRecord = D("TeachingRecord", "Logic")->insertTeachingRecord(I("transactionId"), I("isInitial"));
+	}
+	
+	public function batchGenerateTeachingRecord(){
+		$transaction = D("Transaction");
+		$map["teachingFrequency"] = array('neq','');
+		$map["fixedTime"] = array('neq','');
+		$map["status"] = array('in',array('5','S'));
+		$data = $transaction->where($map)->select();
+		for($i = 0; $i < count($data); $i++){
+			D("TeachingRecord", "Logic")->insertTeachingRecord($data[$i]["transactionId"], 'N');
+		}
+	}
 }
