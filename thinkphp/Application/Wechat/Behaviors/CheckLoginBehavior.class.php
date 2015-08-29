ab345d8ec29feddc52b648917295c7b485b717b0
ZhengHuahua <sven.zhenghh@gmail.com>
2015-08-29 16:54:59 +0800

    open test mode

--- thinkphp/Application/Wechat/Behaviors/CheckLoginBehavior.class.php
+++ thinkphp/Application/Wechat/Behaviors/CheckLoginBehavior.class.php
@@ -18,8 +18,8 @@ class CheckLoginBehavior extends \Think\Behavior{
 		}else{
 			//need to be modified to show hint and qrcode image
 		    //echo "NO CODE";
-		    //$openid = "obS35vk9Hqwl4WZXsosjxm_hckKQ";
-			exit("OpenId not provided");
+		    $openid = "obS35vk9Hqwl4WZXsosjxm_hckKQ";
+			//exit("OpenId not provided");
 			//$this->error('非法访问，请从微信公众号菜单访问。');
 		}
 		session('openid', $openid);
