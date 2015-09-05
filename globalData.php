df5f578f8f08257e2d4059ff8e374107aa407348
dingjunnan <1415609649@qq.com>
2015-09-05 14:28:20 +0800

    globaldata.php
    for testing

--- globalData.php
+++ globalData.php
@@ -7,7 +7,7 @@
 		private $rootUrl = "www.ilearnnn.com";
 		private $wechatAppId = "wx9855e946fbde03ac";
 		private $wechatAppSecret = "28e4b6e745a58b2999afee567478b105";
-		private $whiteList= "('obS35vk9Hqwl4WZXsosjxm_hckKQ',
+		private $whiteList= /*"('obS35vk9Hqwl4WZXsosjxm_hckKQ',
 										'obS35vukOJDFCh2RulSiTq6bVFPI',
 										'obS35vu4p_zaCQaZXWSRJ2ZeWRXY',
 										'obS35vrF0AKDNL7MPB-ldm9feeyU',
@@ -23,7 +23,8 @@
 										'obS35vk0ok-8JHZcaMloqre27g2Y',
 										'obS35vufAmy_XwRlf6KOEYCtlzGE',
 										'',
-										'obS35vpbcdKbDmBe1hy0L-nZlq0k')";
+										'obS35vpbcdKbDmBe1hy0L-nZlq0k')";*/
+							"()";
 		public function getMysqlHost(){
 			return $this->mysqlHost;
 		}
